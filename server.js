5280f753eec947dbcd1a2911989184f7a4588ab2
yutveg <kodi.0n31l@gmail.com>
2020-01-21 14:23:53 -0800

    fully functional

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 
+const carRouter = require("./api/carRouter");
+
 const server = express();
 
 server.use(express.json());
+server.use("/api/cars", carRouter);
 
 module.exports = server;
